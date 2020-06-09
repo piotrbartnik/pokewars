13bb11e16d965b317ab204117436dfe852f0c9c9
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-06-09 14:03:52 +0200

    chore: created simple architecture

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from "react";
+import styles from "./App.module.scss";
+
+const App = (): ReactElement => {
+  return <div className={styles.App}>test</div>;
+};
+
+export default App;
