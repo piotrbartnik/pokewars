687c4d9fcdd010d586d43c2c228aefe185cb67aa
Piotr Bartnik <piotr.bartnik@hotmail.com>
2020-06-11 23:19:24 +0200

    feat: added react-dnd with dragable card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 import styles from "./Card.module.scss";
 
 type CardProps = {
   pokemonName: string;
 };
 
+export const ItemTypes = {
+  CARD: "card",
+};
+
 const Card: React.FC<CardProps> = ({ pokemonName }) => {
-  return <div className={styles.card}>{pokemonName}</div>;
+  const [, drag] = useDrag({
+    item: { type: ItemTypes.CARD },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+  return (
+    <div ref={drag} className={styles.card}>
+      {pokemonName}
+    </div>
+  );
 };
 
 export default Card;
